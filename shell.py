3bb7ba20b9d00650bcca11193ac47a9853d9a8da
Cristian <camposcristian.cjc@gmail.com>
2024-03-28 21:06:11 -0400

    Added tabs and text input functionality
    Co-authored-by: Jaden Passero

--- shell.py
+++ shell.py
@@ -3,44 +3,17 @@ from io import StringIO
 from lexer import Lexer
 from memory import Memory
 from recursiveDescentParser import Parser
-import streamlit as st
 
-st.title("Broccoli")
 # global mem instance
 memory = Memory()
 parser = Parser(memory)
-#while True:
-#    text = input("broccoli > ")
-#    if text == "":
-#        continue
-# filename = "test.txt"
-def onClick(filename):
-    if filename is None:
-        return
-    # using streamlit and StringIO to read file
-    stringIO = StringIO(filename.getvalue().decode('utf-8'))
-    text = stringIO.read()
-    lexer = Lexer(text)
-    tokenList = lexer.tokenize() # this call should not be needed?
-    print(tokenList)
-    parser.setTokens(tokenList)
-    parser.parse()
-    st.header(parser.evaluate())
 
-filename = st.file_uploader("Upload a file", type="txt")
-
-onClick(filename)
-# def main():
-#     parser = Parser()
-#     filename = sys.argv[1]
-#     with open(filename, 'r') as file:
-#         text = file.read()
-#     lexer = Lexer(text)
-#     tokenList = lexer.tokenize()
-#     parser.setTokens(tokenList)
-#     print(lexer.lineTokens)
-#     parser.parse()
-#     print(parser.evaluate())
-
-# if __name__ == '__main__':
-#     main()
+filename = "test.txt"
+with open(filename, 'r') as file:
+    text = file.read()
+lexer = Lexer(text)
+tokenList = lexer.tokenize() # this call should not be needed?
+print(tokenList)
+parser.setTokens(tokenList)
+parser.parse()
+print(parser.evaluate())
